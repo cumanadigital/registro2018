7cffc8e8a0eea2b1d5faf66b6f235a1d156420d5
Oswaldo Hernandez <oswaldoehc@gmail.com>
2018-04-03 22:58:00 -0400

    modificado comportamientos del formulario y la validación

--- controladores/controller.admin.gestionar.planteles.personal.comision.servicio.js
+++ controladores/controller.admin.gestionar.planteles.personal.comision.servicio.js
@@ -152,136 +152,56 @@ $(function () {
 	    accion = "agregar_personal_comision_servicio";
 	    console.info(accion);
 	    // // console.log($(this).attr('id') + " --> " +  $(this).text());          
-
 	    $('#form_modal_personal_comision_servicio').bootstrapValidator('validate');
 	    var isValidForm = $('#form_modal_personal_comision_servicio').data('bootstrapValidator').isValid();
-	    // alert(isValidForm);
-	    
-	    // var id_plantelesbase = $modal_personal.find('input[name="txt_id_plantelesbase"]').val();
-	    // var cod_plantel = $modal_personal.find('input[name="txt_cod_plantel_per"]').val();
-	    // var cedula_personal = $modal_personal.find('input[name="txt_cedula_personal"]').val();
 
-	    // var nombre      = $modal_personal.find('input[name="txt_nombre_funcionario"]').val();
-	    // var apellido  = $modal_personal.find('input[name="txt_apellido_funcionario"]').val();
-
-	    // var sexo        = $("#txt_sexo_funcionario").val();
-	    // var civil       = $("#txt_edocivil_funcionario").val();
-
-	    // var fecnac      = $modal_personal.find('input[name="txt_fechanac_funcionario"]').val();
-	    // var celular     = $modal_personal.find('input[name="txt_celular_funcionario"]').val();
-	    // var telefon     = $modal_personal.find('input[name="txt_telefono_funcionario"]').val();
-	    // var correo      = $modal_personal.find('input[name="txt_correo_funcionario"]').val();
-	    // var twitter     = $modal_personal.find('input[name="txt_twitter_funcionario"]').val();
-	    // var direccion   = $modal_personal.find('input[name="txt_direccion_funcionario"]').val();
-
-	    // var grado       = $("#txt_grado_instruccion").val();
-	    // var titulo      = $modal_personal.find('input[name="txt_titulo"]').val();
-	    // var instit      = $modal_personal.find('input[name="txt_institucion"]').val();
-
-	    // var discapa     = $("#txt_discapacidad").val()  || [];
-	    // var discapatx   = discapa.join( ", " );
-
-
-	    // // // var multipleVal = $("#txt_discapacidad").val() || [];
-	    // // // alert(discapa);
-	    // // // var multivv     = multipleVal.join( ", " )
-	    // // // console.info(discapa);
-	    // // // console.info(discapatx);
-	    // // // $("#txt_discapacidad").val(discapatx);
-	    // var otra_disca  = $modal_personal.find('input[name="txt_discapacidad_otra"]').val();
-
-	    // var tipoper     = $("#txt_tipo_personal_funcional").val();
-	    // var tipofun     = $modal_personal.find('select[name="txt_tipo_personal_funcional"]').val();
-	    // var cargof     = $modal_personal.find('input[name="txt_cargo_funcion"]').val();
-	    // var coordl     = $modal_personal.find('input[name="txt_coordinacion_laboral"]').val();
-	    // var turno     = $("#txt_turno_trabajo").val();
-
-	    // var horario     = $modal_personal.find('input[name="txt_horario_laboral"]').val();
-	    // var horasdoc     = $modal_personal.find('input[name="txt_horas_doc_funcionario"]').val();
-	    // var horadadm     = $modal_personal.find('input[name="txt_horas_adm_obr_funcionario"]').val();
-
-	    // var fechaing     = $modal_personal.find('input[name="txt_fecha_ingreso"]').val();
-	    // var tiempo      = $modal_personal.find('input[name="txt_tiempo_servicio"]').val();
-
-	    // // descativamos la cedula
-	    // $("#txt_cedula_personal").attr('readonly',true);
-	    // // console.info($("#form_modal_personal").serializeArray());
-	    
-	    // // if (cedula_personal!=''  ) {
-	    // // if(isValidForm){
-	    // //   // $('#form_modal_personal').data('formValidation').resetForm();
-	    // //   alert("mas fino es valido")  
-	    // // }else{
-	    // //   alert("oh oh")
-	    // // }
-
-	    // // if (cedula_personal!='' && nombre!='' && apellido!= '' && sexo!= '' && civil!= '' && fecnac!= '' && celular!= '' && telefon!= '' && direccion!= '' && grado!= '' && titulo!= '' && tipoper!= '' && tipofun!= '' && cargof!= '' && coordl!= '' && turno!= '' && horario!= '' && horasdoc!= '' && horadadm!= '' && fechaing!= '' && tiempo!= ''  ) {
-	    
-	    // // $('#registrationForm').bootstrapValidator({
-	    // //     // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
-	    // //     feedbackIcons: {
-	    // //         valid: 'glyphicon glyphicon-ok',
-	    // //         invalid: 'glyphicon glyphicon-remove',
-	    // //         validating: 'glyphicon glyphicon-refresh'
-	    // //     },
-	    // //     fields: {
-	    // //         birthday: {
-	    // //             validators: {
-	    // //                 notEmpty: {
-	    // //                     message: 'The date of birth is required'
-	    // //                 },
-	    // //                 date: {
-	    // //                     format: 'YYYY/MM/DD',
-	    // //                     message: 'The date of birth is not valid'
-	    // //                 }
-	    // //             }
-	    // //         }
-	    // //     }
-	    // // });
-	    // // });
-
+	    // isValidForm = true;
 	    if(isValidForm){
-	    //   $("#btn_enviar_personal").attr('disabled', true);
-	    //   parametros = $("#form_modal_personal").serialize() + '&accion='+ accion;
-	      
-	    //   API_URL =  "servicios/services.admin.planteles.php";
-	    //   // alert(parametros);
-	    //   // console.log(parametros);
-	    //   $.ajax({
-	    //       url: API_URL + ($modal_personal.data('id') || ''),
-	    //       type: 'POST',
-	    //       data: parametros + "&token1="+rand_code(),
-	    //       success: function (response) {
-	    //           // $modal_personal.modal('hide');
-	    //           // ocultar  cuadros
-	              
-	    //           desactivar_datos_personales();
-	              
-	    //           $('#cuadro_datos_laborales').hide();
-	    //           $('#cuadro_datos_personal').hide();
-	    //           $('#cuadro_listado_personal').fadeIn();
+	    	// DESACTIVAMO SEL BOTON ENVIAR
+	    	$("#btn_enviar_personal_comision_servicio").attr('disabled', true);
+	    	
+	    	parametros = $("#form_modal_personal_comision_servicio").serialize() + '&accion='+ accion;
+	      	// DESACTIVAMOS TODO EL FORMULARIO
+	      	$('#cuadro_comision_de_servicio').find('input, textarea, button, select').attr('disabled','disabled');
 
-	    //           $('#btn_volver_listado').attr('disabled',true);
-	    //           $('#btn_enviar_personal').attr('disabled',true);
+	    	API_URL =  "servicios/services.admin.planteles.php";
+		    // alert(parametros);
+		    console.log(parametros);
+			$.ajax({
+				url: API_URL + ($modal_personal.data('id') || ''),
+				type: 'POST',
+				data: parametros + "&token1="+rand_code(),
+				success: function (response) {
+					// $modal_personal.modal('hide');
+					// ocultar  cuadros
 
-	    //           $('#btn_volver_listado').hide();
-	    //           $('#btn_enviar_personal').hide();
+					// desactivar_datos_personales();
 
-	    //           // showAlert('Registro con éxito!', 'success');
+					// $('#cuadro_datos_laborales').hide();
+					// $('#cuadro_datos_personal').hide();
+					// $('#cuadro_listado_personal').fadeIn();
 
-	    //           $table.bootstrapTable('refresh');
-	    //           $('#table_personal_asignado').bootstrapTable('refresh');
-	    //           //
-	    //           // destruimos la validacion
-	    //           // $('#form_modal_personal').validator('destroy');
-	    //           $('#form_modal_personal').bootstrapValidator('resetForm', true);
-	    //           
-	    //       },
-	    //       error: function () {
-	    //           // $modal_personal.modal('hide');
-	    //           showAlert(($modal_personal.data('id') ? 'Update' : 'Create') + ' item error!', 'danger');
-	    //       }
-	    //   });
+					// $('#btn_volver_listado').attr('disabled',true);
+					// $('#btn_enviar_personal').attr('disabled',true);
+
+					// $('#btn_volver_listado').hide();
+					// $('#btn_enviar_personal').hide();
+
+					showAlert('Registro con éxito!', 'success');
+
+					// $table.bootstrapTable('refresh');
+					// $('#table_personal_asignado').bootstrapTable('refresh');
+					// //
+					// // destruimos la validacion
+					// // $("#form_modal_personal_comision_servicio").validator('destroy');
+					// $("#form_modal_personal_comision_servicio").bootstrapValidator('resetForm', true);
+
+				},
+				error: function () {
+					// $modal_personal.modal('hide');
+					showAlert(($modal_personal.data('id') ? 'Update' : 'Create') + ' item error!', 'danger');
+				}
+			});
 	    }else{
 	    	alert("Debe verificar los datos ingresados");
 	    }
